b059b4db557b07bb81b18b73d6047d88bd784165
BK <bhargavvora9@gmail.com>
2026-09-08 17:13:10 +0530

    Complete light storefront theme across all customer-facing routes

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,7 @@ import './globals.css';
 import './product-modal.css';
 import './product-gallery-fix.css';
 import './pinkbox-radiant-theme.css';
+import './pinkbox-storefront-light.css';
 import PinkBoxProductGalleryFix from '@/components/PinkBoxProductGalleryFix';
 
 export const metadata = {
