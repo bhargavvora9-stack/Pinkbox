1406b90344a0e0c0bf8ef98ee9c12dd2304a0d85
BK <bhargavvora9@gmail.com>
2026-09-14 18:55:30 +0530

    Enforce no-crop contain rendering for all website images

--- components/WebsiteImageDisplayFix.js
+++ components/WebsiteImageDisplayFix.js
@@ -5,18 +5,14 @@ export default function WebsiteImageDisplayFix(){
  useEffect(()=>{
   const apply=()=>{
    document.querySelectorAll('img').forEach(img=>{
-    const src=img.currentSrc||img.src||'';
-    if(src.includes('/storage/v1/object/public/website-images/')){
-      img.style.objectFit='contain';
-      img.style.objectPosition='center';
-      img.style.maxWidth='100%';
-      if(img.closest('.pb-home')) img.style.backgroundColor=img.style.backgroundColor||'transparent';
-    }
+    img.style.objectFit='contain';
+    img.style.objectPosition='center';
+    img.style.maxWidth='100%';
    });
   };
   apply();
   const observer=new MutationObserver(apply);
-  observer.observe(document.body,{childList:true,subtree:true,attributes:true,attributeFilter:['src']});
+  observer.observe(document.body,{childList:true,subtree:true,attributes:true,attributeFilter:['src','class','style']});
   return()=>observer.disconnect();
  },[]);
  return null;
